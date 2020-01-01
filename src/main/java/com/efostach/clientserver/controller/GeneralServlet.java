978d72142a5a87fcf34b97a46d8703aad54c0593
Helen Fostach <elena.fostach@gmail.com>
2020-01-01 23:17:46 +0200

    added servlet config file, jsp files and servlet class

--- src/main/java/com/efostach/clientserver/controller/GeneralServlet.java
+++ src/main/java/com/efostach/clientserver/controller/GeneralServlet.java
@@ -6,15 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "GeneralServlet", urlPatterns = "/")
 public class GeneralServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+        request.getRequestDispatcher("menu.jsp").forward(request, response);
     }
 
     @Override
--- src/main/webapp/menu.jsp
+++ src/main/webapp/menu.jsp
@@ -1,9 +1,9 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<head>
-    <title>Menu</title>
-</head>
-<body>
-<h1>Hello</h1>
-</body>
+    <head>
+        <title>Menu</title>
+    </head>
+    <body>
+        <h1>Hello</h1>
+    </body>
 </html>
