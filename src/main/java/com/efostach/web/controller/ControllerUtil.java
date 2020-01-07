093b714ba7b5d5f366962d25fd81313a9c9ef740
Helen Fostach <elena.fostach@gmail.com>
2020-01-07 02:22:51 +0200

    code refactoring

---
+++ src/main/java/com/efostach/web/controller/ControllerUtil.java
@@ -0,0 +1,8 @@
+package com.efostach.web.controller;
+
+public class Util {
+
+    public static String EMPTY_INPUT_VALUE_WARN_MSG = "Please, input value.";
+    public static String OBJ_NOT_FOUND_WARN_MSG = "Object is not found.";
+    public static String REF_OBJ_NOT_EXISTS_WARN_MSG = "Input referenced object doesn't exist.";
+}
--- src/main/java/com/efostach/web/controller/Util.java
+++
@@ -1,7 +0,0 @@
-package com.efostach.web.controller;
-
-public class Util {
-
-    public static String EMPTY_INPUT_VALUE_WARN_MSG = "Please, input value.";
-    public static String OBJ_NOT_FOUND_WARN_MSG = "Object not found.";
-}
